ff595d0635ee12c4858afd0596ad0e5c70ae1fdd
Sugonyak Kristina <sugonyak@tutu.tech>
2023-08-22 15:58:03 +0200

    added game history

--- src/App.js
+++ src/App.js
@@ -55,15 +55,31 @@ function Board({onPlay, xIsNext, squares}) {
 export default function Game() {
 const [xIsNext, setXIsNext] = useState(true);
 const [history, setHistory] = useState([Array(0).fill(null)]);
+const [currentMove, setCurrentMove] = useState(0);
 
-const currentSquares = history[history.length - 1];
+const currentSquares = history[currentMove];
 
 function handlePlay(nextSquares) {
-  setHistory([...history, nextSquares])
+  const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+  setHistory(nextHistory);
+  setCurrentMove(nextHistory.length - 1);
   setXIsNext(!xIsNext)
+}
 
+function jumpTo(nextMove) {
+  setCurrentMove(nextMove);
+  setXIsNext(nextMove % 2 === 0);
 
 }
+
+const moves = history.map((squares, index) => {
+  let moveName;
+  moveName = index > 0 ? 'Go to move #' + index : 'Go to game start';
+
+  return (
+    <li key={index}><button onClick={()=>jumpTo(index)}>{moveName}</button></li>
+  )
+})
 
   return (
     <div className="game">
@@ -71,7 +87,7 @@ function handlePlay(nextSquares) {
         <Board onPlay={handlePlay} xIsNext={xIsNext} squares={currentSquares}/>
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
