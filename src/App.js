f20ebedab83f7839de67d7616b2c6240640d9e0c
Sugonyak Kristina <sugonyak@tutu.tech>
2023-08-22 16:08:47 +0200

    deleted redundant state xIsNext

--- src/App.js
+++ src/App.js
@@ -53,9 +53,10 @@ function Board({onPlay, xIsNext, squares}) {
 }
 
 export default function Game() {
-const [xIsNext, setXIsNext] = useState(true);
 const [history, setHistory] = useState([Array(0).fill(null)]);
 const [currentMove, setCurrentMove] = useState(0);
+
+const xIsNext = currentMove % 2 === 0;
 
 const currentSquares = history[currentMove];
 
@@ -63,12 +64,10 @@ function handlePlay(nextSquares) {
   const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
   setHistory(nextHistory);
   setCurrentMove(nextHistory.length - 1);
-  setXIsNext(!xIsNext)
 }
 
 function jumpTo(nextMove) {
   setCurrentMove(nextMove);
-  setXIsNext(nextMove % 2 === 0);
 
 }
 
